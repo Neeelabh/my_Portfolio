f04ff237e7e81cd30d1dfbb111cd375236f95890
neelabh <neelabhmkd@gmail.com>
2025-05-02 18:12:36 +0530

    tech stack issue .. again..

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -7,8 +7,7 @@ import {
 } from "react-icons/di";
 import {
   SiCplusplus,
-  SiMicrosoftsqlserver,
-  SiMicrosoftexcel,
+  SiMicrosoftExcel,
   SiTableau,
 } from "react-icons/si";
 import {
@@ -20,36 +19,15 @@ import {
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiCplusplus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMicrosoftsqlserver />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTableau />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMicrosoftexcel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <PiFileSql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <PiMicrosoftWordLogoBold />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <PiTrademarkRegisteredBold />
-      </Col>
+      <Col xs={4} md={2} className="tech-icons"><DiPython /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiCplusplus /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiMongodb /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiGit /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiMicrosoftExcel /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiTableau /></Col>
+      <Col xs={4} md={2} className="tech-icons"><PiFileSql /></Col>
+      <Col xs={4} md={2} className="tech-icons"><PiMicrosoftWordLogoBold /></Col>
+      <Col xs={4} md={2} className="tech-icons"><PiTrademarkRegisteredBold /></Col>
     </Row>
   );
 }
